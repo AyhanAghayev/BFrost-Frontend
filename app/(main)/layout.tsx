dec17fc1a4a0a4d7531be3e5e4701b3a2124c1ac
BillerPlay <abulyaa@gmail.com>
2026-07-09 03:01:49 +0400

    feat: add TopNavBar component

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import TopNavBar from "@/components/layout/TopNavBar";
 import AuthGuard from "@/components/layout/AuthGuard";
 
 export default function MainLayout({
@@ -7,6 +8,7 @@ export default function MainLayout({
 }) {
   return (
     <AuthGuard>
+      <TopNavBar />
       <div className="w-full max-w-container-max mx-auto flex min-w-0">
         {children}
       </div>
